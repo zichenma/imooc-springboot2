4b8c1a1ddca6f2da9bfe8594b84a93e43f2711ff
zichen ma <zichenma@zichens-MacBook-Pro.local>
2022-11-26 23:29:28 -0500

    finished 3-7

--- src/main/java/org/example/pojo/bo/TeacherBO.java
+++ src/main/java/org/example/pojo/bo/TeacherBO.java
@@ -4,16 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 
 @Data
 @ToString
 @AllArgsConstructor
 public class TeacherBO {
     private String id;
-
+    @NotBlank
     private String name;
-
     private Integer age;
-
+    @NotNull
     private Integer sex;
 }
