ce47867d1825eda30b85a1a0f778881cc2c1b82a
zichen ma <zichenma@zichens-MacBook-Pro.local>
2022-11-27 11:45:12 -0500

    finished 3-9

--- src/main/java/org/example/service/TeacherService.java
+++ src/main/java/org/example/service/TeacherService.java
@@ -2,10 +2,20 @@ package org.example.service;
 
 import org.example.pojo.Teacher;
 
+import java.util.List;
+
 public interface TeacherService {
     /**
      * 新增 Teacher 到数据库
      * @param teacher
      */
     public void saveTeacher(Teacher teacher);
+
+    /**
+     * 根据条件查询 teacher 的 list 结果集
+     * @param id
+     */
+    public Teacher queryById(String id);
+
+    public List<Teacher> queryByCondition(String name, Integer sex);
 }
--- src/main/java/org/example/service/impl/TeacherServiceImpl.java
+++ src/main/java/org/example/service/impl/TeacherServiceImpl.java
@@ -5,6 +5,9 @@ import org.example.pojo.Teacher;
 import org.example.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
 
 // 既然是 service 的实现，而且需要放到 springboot 容器里，供 controller 调用, 所以需要 @Service
 @Service
@@ -16,5 +19,35 @@ public class TeacherServiceImpl implements TeacherService {
     @Override
     public void saveTeacher(Teacher teacher) {
         teacherMapper.insert(teacher);
+    }
+
+    @Override
+    public Teacher queryById(String id) {
+        Teacher teacher = teacherMapper.selectByPrimaryKey(id);
+        return teacher;
+    }
+
+    // 根据一个条件去进行查询
+
+//    @Override
+//    public List<Teacher> queryByCondition(String name, Integer sex) {
+//        Example example = new Example(Teacher.class);
+//        Example.Criteria criteria = example.createCriteria();
+//        //相当于 WHERE name={name} AND sex={sex}
+//        criteria.andEqualTo("name", name);
+//        criteria.andEqualTo("sex", sex);
+//        List<Teacher> teacherList = teacherMapper.selectByExample(example);
+//        return teacherList;
+//    }
+
+    // 根据一个条件对象去进行查询
+    @Override
+    public List<Teacher> queryByCondition(String name, Integer sex) {
+        // 条件对象 (只适用于严格查询, 模糊查询等则需要用 Criteria）
+        Teacher teacher = new Teacher();
+        teacher.setName(name);
+        teacher.setSex(sex);
+        List<Teacher> teacherList = teacherMapper.select(teacher);
+        return teacherList;
     }
 }
