73c85fccfe570fb20d016218a8effffea6710849
zichen ma <zichenma@zichens-MacBook-Pro.local>
2022-12-24 20:54:35 -0500

    5_1 dded jpa plugin

---
+++ src/test/java/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    int calculateDaysBetweenDates(String date1, String date2) {
+        // 1. 将两个日期字符串转换为日期对象
+        // 2. 通过日期对象获取到时间戳
+        // 3. 通过时间戳计算两个日期之间的天数
+        // 4. 返回天数
+        return 0;
+    }
+}
+
+
+
+
